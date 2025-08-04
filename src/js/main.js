ac96d2dcde29954ad461a537d83e88d84ca312ce
Max <dev.max.frontend.js@gmail.com>
2025-08-04 21:41:10 +0300

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,11 @@ $(document).ready(function () {
         const targetElement = $(targetId);
 
         if (targetElement.length) {
-            const targetOffset = targetElement.offset().top - 20;
+            if (window.innerWidth <= 425) {
+                targetOffset = targetElement.offset().top - 94;
+            } else {
+                targetOffset = targetElement.offset().top - 20;
+            }
 
             $('html, body').animate({
                 scrollTop: targetOffset
@@ -15,49 +19,66 @@ $(document).ready(function () {
             });
         }
     });
-});
 
-
-document.addEventListener("DOMContentLoaded", function () {
+    //Slider
     const slider = document.querySelector(".partners__slider");
     const clone = slider.innerHTML;
     slider.innerHTML += clone;
-});
 
+    //FAQ
+    const $accordionItems = $('.faq__list-item');
+    const $allIcons = $('.faq__list-icon-wrapper .faq__list-icon');
+    const $allTexts = $('[data-text]');
 
-const $accordionItems = $('.faq__list-item');
-const $allIcons = $('.faq__list-icon-wrapper .faq__list-icon');
-const $allTexts = $('[data-text]');
+    $accordionItems.on('click keydown', function (e) {
+        if (e.type === 'click' || (e.type === 'keydown' && (e.key === 'Enter' || e.keyCode === 13))) {
+            const $currentItem = $(this);
+            const $currentIcon = $currentItem.find('.faq__list-icon-wrapper .faq__list-icon');
+            const $currentText = $currentItem.find('[data-text]');
+            const isOpen = $currentIcon.hasClass('open');
 
-$accordionItems.on('click keydown', function (e) {
-    if (e.type === 'click' || (e.type === 'keydown' && (e.key === 'Enter' || e.keyCode === 13))) {
-        const $currentItem = $(this);
-        const $currentIcon = $currentItem.find('.faq__list-icon-wrapper .faq__list-icon');
-        const $currentText = $currentItem.find('[data-text]');
-        const isOpen = $currentIcon.hasClass('open');
+            if (isOpen) {
+                $currentIcon.removeClass('open');
+                $currentText.stop(true, true).animate({opacity: 0}, 150, function () {
+                    $(this).slideUp(200);
+                });
+            } else {
+                $allIcons.removeClass('open');
+                $allTexts.stop(true, true).animate({opacity: 0}, 150, function () {
+                    $(this).slideUp(200);
+                });
 
-        if (isOpen) {
-            $currentIcon.removeClass('open');
-            $currentText.stop(true, true).animate({ opacity: 0 }, 150, function () {
-                $(this).slideUp(200);
-            });
-        } else {
-            $allIcons.removeClass('open');
-            $allTexts.stop(true, true).animate({ opacity: 0 }, 150, function () {
-                $(this).slideUp(200);
-            });
-
-            $currentIcon.addClass('open');
-            $currentText
-                .stop(true, true)
-                .css({ display: 'none', opacity: 0 })
-                .slideDown(200)
-                .animate({ opacity: 1 }, 400);
+                $currentIcon.addClass('open');
+                $currentText
+                    .stop(true, true)
+                    .css({display: 'none', opacity: 0})
+                    .slideDown(200)
+                    .animate({opacity: 1}, 400);
+            }
         }
-    }
-});
+    });
 
-document.addEventListener("DOMContentLoaded", () => {
+    //Anim
     const aboutImg = document.querySelector('.about__img');
     aboutImg.classList.add('animate');
+
+    //Menu
+    $('.header__mobile-burger').click(function (e) {
+        e.preventDefault();
+
+        $('.header__mobile-menu').toggleClass('open');
+
+        if (!$(this).hasClass('cross')) {
+            $(this).addClass('cross');
+            $('.header').addClass("open");
+        } else {
+            $(this).removeClass('cross');
+            $('.header').removeClass("open");
+        }
+    });
+
+    //Header
+    $('.header__mobile-menu a').on('click', function () {
+        $('.header__mobile-burger').trigger('click');
+    });
 });
